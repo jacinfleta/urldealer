a32da474a8554ffd04dd8dbb40fbcc7ab051c4f0
Jacin Fleta de Castro <jacintofleta@gmail.com>
2020-03-08 12:50:47 +0100

    New

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function redirect(visitNumber){
+
+        caseNumber = visitNumber % 3;
+        console.log(visitNumber);
+        
+        switch (caseNumber) {
+            case 0:
+                window.location = "http://google.com";
+                break;
+            case 1:
+                window.location = "http://facebook.com";
+                break;
+            case 2:
+                window.location = "http://twitter.com";
+                break;
+        }
+};
